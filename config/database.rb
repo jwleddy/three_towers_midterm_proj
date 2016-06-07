b9727f29fedc804d1b79aaa8d9b746b084e5fcde
James Leddy <jwleddy@gmail.com>
2016-06-07 14:24:43 -0700

    changed database.rb

--- config/database.rb
+++ config/database.rb
@@ -12,17 +12,9 @@ configure :development do
 end
 
 configure :production do
-  db = URI.parse(ENV['DATABASE_URL'] || 'postgres:///localhost/mydb')
-
- ActiveRecord::Base.establish_connection(
-   :adapter  => db.scheme == 'postgres' ? 'postgresql' : db.scheme,
-   :host     => db.host,
-   :username => db.user,
-   :password => db.password,
-   :database => db.path[1..-1],
-   :encoding => 'utf8'
- )
+  set :database, ENV['DATABASE_URL']
 end
+
 configure do
   # Load all models from app/models, using autoload instead of require
   # See http://www.rubyinside.com/ruby-techniques-revealed-autoload-1652.html

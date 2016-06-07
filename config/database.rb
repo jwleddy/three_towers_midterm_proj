442bc48e6495691e9e1437d3351509fceebc6662
jwleddy <jwleddy@gmail.com>
2016-06-07 05:21:19 +0000

    changed prod db settings

--- config/database.rb
+++ config/database.rb
@@ -1,14 +1,29 @@
-configure do
+configure :development do
   # Log queries to STDOUT in development
   if Sinatra::Application.development?
     ActiveRecord::Base.logger = Logger.new(STDOUT)
   end
 
-  set :database, {
-    adapter: "postgresql",
-    database: "three_towers_db"
-  }
 
+  set :database, {
+    adapter: "sqlite3",
+    database: "db/db.sqlite3"
+  }
+end
+
+configure :production do
+  db = URI.parse(ENV['DATABASE_URL'] || 'postgres:///localhost/mydb')
+
+ ActiveRecord::Base.establish_connection(
+   :adapter  => db.scheme == 'postgres' ? 'postgresql' : db.scheme,
+   :host     => db.host,
+   :username => db.user,
+   :password => db.password,
+   :database => db.path[1..-1],
+   :encoding => 'utf8'
+ )
+end
+configure do
   # Load all models from app/models, using autoload instead of require
   # See http://www.rubyinside.com/ruby-techniques-revealed-autoload-1652.html
   Dir[APP_ROOT.join('app', 'models', '*.rb')].each do |model_file|

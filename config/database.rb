e9e436e4128bcce790e6af358b637864ecb5bb88
James Leddy <jwleddy@gmail.com>
2016-06-07 14:26:31 -0700

    changed database.rb again

--- config/database.rb
+++ config/database.rb
@@ -1,21 +1,18 @@
-configure :development do
+configure do
   # Log queries to STDOUT in development
   if Sinatra::Application.development?
     ActiveRecord::Base.logger = Logger.new(STDOUT)
   end
 
-
+if development?
   set :database, {
     adapter: "sqlite3",
     database: "db/db.sqlite3"
   }
-end
-
-configure :production do
+else
   set :database, ENV['DATABASE_URL']
 end
 
-configure do
   # Load all models from app/models, using autoload instead of require
   # See http://www.rubyinside.com/ruby-techniques-revealed-autoload-1652.html
   Dir[APP_ROOT.join('app', 'models', '*.rb')].each do |model_file|
